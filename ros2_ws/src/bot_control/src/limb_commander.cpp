7bc0baf404ee3d7d2ef6039f641e3040b84f46ef
andriibessarab <contact@andriibessarab.com>
2026-04-29 16:19:01 -0400

    feat(ros/bot_control): init action client in limb_commander

--- ros2_ws/src/bot_control/src/limb_commander.cpp
+++ ros2_ws/src/bot_control/src/limb_commander.cpp
@@ -1,13 +1,27 @@
 #include "rclcpp/rclcpp.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
+#include "control_msgs/action/follow_joint_trajectory.hpp"
 #include <memory>
 
 class LimbCommander : public rclcpp::Node
 {
 public:
+    // alias
+    using FollowJointTrajectory = control_msgs::action::FollowJointTrajectory;
+
     LimbCommander() : Node("limb_controller")
     {
         RCLCPP_INFO(this->get_logger(), "Limb Commander node has started!");
+
+        // init action client
+        action_client_ = rclcpp_action::create_client<FollowJointTrajectory>(
+            this,
+            "/joint_trajectory_controller/follow_joint_trajectory"
+        );
     }
+
+private:
+    rclcpp_action::Client<FollowJointTrajectory>::SharedPtr action_client_;  // action client ptr
 };
 
 int main(int argc, char **argv)
