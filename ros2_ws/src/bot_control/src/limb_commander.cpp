ccf66f7cf26692edd7582de23ede92f163e9e66d
andriibessarab <contact@andriibessarab.com>
2026-04-30 03:40:17 -0400

    feat(ros/bot_control): create send_goal method that waits for action server to start and assigns joints in goal.trajectory

--- ros2_ws/src/bot_control/src/limb_commander.cpp
+++ ros2_ws/src/bot_control/src/limb_commander.cpp
@@ -2,6 +2,13 @@
 #include "rclcpp_action/rclcpp_action.hpp"
 #include "control_msgs/action/follow_joint_trajectory.hpp"
 #include <memory>
+#include <vector>
+#include <chrono>
+#include <string>
+
+// param names
+const std::string JOINTS_PARAM_NAME = "joints";
+const std::string ACTION_SERVER_NAME_PARAM_NAME = "action_server_name";
 
 class LimbCommander : public rclcpp::Node
 {
@@ -13,15 +20,46 @@ public:
     {
         RCLCPP_INFO(this->get_logger(), "Limb Commander node has started!");
 
+        // declare params
+        this->declare_parameter<std::string>(ACTION_SERVER_NAME_PARAM_NAME, std::string{});
+        this->declare_parameter<std::vector<std::string>>(JOINTS_PARAM_NAME, std::vector<std::string>{});  // joints
+
+        // get params
+        action_server_name_ = this->get_parameter(ACTION_SERVER_NAME_PARAM_NAME).as_string();
+        joints_ = this->get_parameter(JOINTS_PARAM_NAME).as_string_array();
+
         // init action client
-        action_client_ = rclcpp_action::create_client<FollowJointTrajectory>(
+        this->action_client_ = rclcpp_action::create_client<FollowJointTrajectory>(
             this,
-            "/joint_trajectory_controller/follow_joint_trajectory"
+            action_server_name_
         );
+    }
+
+    void send_goal()
+    {
+        // wait for action server to turn on
+        while (!action_client_->wait_for_action_server(std::chrono::milliseconds(1000)))
+        {
+            if (!rclcpp::ok())  // hande interruption
+            {
+                RCLCPP_ERROR(this->get_logger(), "Interrupted while waiting for action server.");
+                return;
+            }
+            RCLCPP_INFO(this->get_logger(), "Action server not available, waiting...");
+        }
+        RCLCPP_INFO(this->get_logger(), "Action server found!");
+
+        auto goal = FollowJointTrajectory::Goal();
+        goal.trajectory.joint_names = this->joints_;
+        
     }
 
 private:
     rclcpp_action::Client<FollowJointTrajectory>::SharedPtr action_client_;  // action client ptr
+
+    //params
+    std::string action_server_name_;
+    std::vector<std::string> joints_;
 };
 
 int main(int argc, char **argv)
