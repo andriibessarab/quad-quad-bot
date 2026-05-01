c57dbe6202708cb68ad13c778d3ad68fc441e340
andriibessarab <contact@andriibessarab.com>
2026-05-01 12:08:21 -0400

    feat(ros/bot_kinematics): add structures in types.hpp for limb dims and joint angles

---
+++ ros2_ws/src/bot_kinematics/include/bot_kinematics/types.hpp
@@ -0,0 +1,27 @@
+#ifndef BOT_KINEMATICS_TYPES_HPP
+#define BOT_KINEMATICS_TYPES_HPP
+
+namespace bot_kinematics {
+
+/**
+ * @brief Physical dimensions of a single quadruped limb.
+ * All measurements are in meters (m).
+ */
+struct LimbDimensions {
+  double l1; // Hip offset
+  double l2; // Femur length
+  double l3; // Tibia length
+};
+
+/**
+ * @brief Standard container for quadruped limb joint angles.
+ * Units: Radians
+ */
+struct LimbJointAngles {
+  double haa;
+  double hfe;
+  double kfe;
+};
+} // namespace bot_kinematics
+
+#endif
