2a60d0d2f334edbdb1f50cc49b02c140b4f0792f
andriibessarab <andriibessarab@gmail.com>
2026-05-04 19:39:53 -0400

    chore(ros/bot_kinematics): implement inverse kinematics for back-left limb

--- ros2_ws/src/bot_kinematics/src/ik_solver.cpp
+++ ros2_ws/src/bot_kinematics/src/ik_solver.cpp
@@ -2,49 +2,57 @@
 
 #include <cmath>
 #include <iostream>
+#include <numbers>
+
+#define _USE_MATH_DEFINES
 
 namespace bot_kinematics {
 IkSolver::IkSolver(const LimbDimensions &dims) : limb_dimensions_(dims) {}
 
   LimbJointAngles IkSolver::calculate_ik(double target_x, double target_y, double target_z) {
   LimbJointAngles angles;
-  const double PI_2 = M_PI / 2.0;
 
   double l1 = limb_dimensions_.l1;
   double l2 = limb_dimensions_.l2;
   double l3 = limb_dimensions_.l3;
 
-  // calculate haa with atan
-  angles.haa = std::atan2(target_y, -target_z);
+  // use derived formulas to calculate the three angles
+  // angles.haa = -(std::atan2(std::sqrt(target_y * target_y + target_z * target_z - l1 * l1), l1) + std::atan2(target_y, -target_z) - M_PI / 2);
+  // angles.kfe = std::acos((target_y * target_y + target_z * target_z - l1 * l1 + target_x * target_x - l2 * l2 - l3 * l3) / (-2 * l2 * l3));
+  // angles.hfe = std::asin((l3 * std::sin(angles.kfe)) / (std::sqrt(target_y * target_y + target_z * target_z - l1 * l1 + target_x * target_x))) + std::atan2(target_x, std::sqrt(target_y * target_y + target_z * target_z - l1 * l1));
 
-  // 2. 2D Projection
-  double z_hypot = std::sqrt(target_y * target_y + target_z * target_z);
-  double z_adj = z_hypot - l1;
-  double D = std::sqrt(target_x * target_x + z_adj * z_adj);
+  /////////////////////////// CALCULATIONS ///////////////////////////
+  // planar distances
+  double d_yz_sq = target_y * target_y + target_z * target_z - l1 * l1;
+  if (d_yz_sq < 0) d_yz_sq = 0; // prevent NaN
+  double d_yz = std::sqrt(d_yz_sq);  // B term from calulations
 
-  // Clamping
-  if (D > (l2 + l3)) D = l2 + l3 - 0.0001;
+  double d_sq = d_yz_sq + target_x * target_x;
+  double d = std::sqrt(d_sq);
 
-  // 3. KFE with your 90-degree offset
-  double cos_gamma = (l2 * l2 + l3 * l3 - D * D) / (2.0 * l2 * l3);
-  double gamma = std::acos(cos_gamma); // Internal angle
+  // HAA
+  angles.haa = -(std::atan2(d_yz, l1) + std::atan2(target_y, -target_z) - M_PI_2);
 
-  // ADJUSTMENT: Center it so 90 degrees = 0 radians
-  angles.kfe = gamma - PI_2;
+  // raw angles
+  double cos_gamma = (l2 * l2 + l3 * l3 - d_sq) / (2 * l2 * l3);
+  cos_gamma = std::max(-1.0, std::min(1.0, cos_gamma));
+  double gamma = std::acos(cos_gamma); // inner knee angle
 
-  // 4. HFE
-  double alpha = std::atan2(target_x, z_adj);
-  double cos_beta = (l2 * l2 + D * D - l3 * l3) / (2.0 * l2 * D);
-  double beta = std::acos(cos_beta);
+  // alpha = angle of femur forward of the straight line to the foot
+  double cos_alpha = (l2 * l2 + d_sq - l3 * l3) / (2 * l2 * d);
+  cos_alpha = std::max(-1.0, std::min(1.0, cos_alpha));
+  double alpha = std::acos(cos_alpha);
 
-  // Since 0 is vertical, we just sum the angles relative to the Z-axis
-  angles.hfe = alpha + beta;
+  // theta = angle of the foot forward of straight down
+  double theta = std::atan2(target_x, d_yz);
 
-  angles.hfe = 0;
-  angles.kfe = 0;
+  // KFE
+  // Bending fwd/up (straightening) is +ve. Zero is L-shape (PI/2).
+  angles.kfe = gamma - M_PI_2;
 
-  std::cout << "IK< ANGLES" << angles.haa << " " << angles.hfe << " " << angles.kfe << std::endl;
-
+  // HFE
+  // 0 rad means  femur points horizontally forward. Bending down is -ve
+  angles.hfe = (M_PI / 2) - (alpha + theta);
 
   return angles;
 }
