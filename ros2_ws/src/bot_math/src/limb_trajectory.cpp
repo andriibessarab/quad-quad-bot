790c4e207db72a7bd0f390f5e7bbbc8e7d9c0669
andriibessarab <andriibessarab@gmail.com>
2026-06-26 15:37:17 -0400

    feat(ros/math): add y direction to trot math

--- ros2_ws/src/bot_math/src/limb_trajectory.cpp
+++ ros2_ws/src/bot_math/src/limb_trajectory.cpp
@@ -19,10 +19,17 @@ LimbTrajectory::compute_target(const LimbTrajectoryInput &input) {
   switch (input.gait_mode) {
     // --- TROT MODE ----
   case GaitMode::Trot: {
-    const double x_home = config_.home_point.x;
-    const double x_fwd = config_.home_point.x + input.step_len;
+    // deadband
+    if (std::abs(input.step_x) < 1e-4 && std::abs(input.step_y) < 1e-4) {
+      return config_.home_point;
+    }
+
+    const double x_init = config_.home_point.x;
+    const double x_final = x_init + input.step_x;
+    const double y_init = config_.home_point.y;
+    const double y_final = y_init + input.step_y;
     const double z_floor = config_.home_point.z;
-    const double z_peak = config_.home_point.z + input.step_height;
+    const double z_peak = z_floor + input.step_height;
 
     const double duty_factor =
         input.stance_duration / (input.stance_duration + input.swing_duration);
@@ -30,16 +37,17 @@ LimbTrajectory::compute_target(const LimbTrajectoryInput &input) {
     // --- Stance Phase ---
     if (limb_phase < duty_factor) {
       const double t = limb_phase / duty_factor;
-      target.x = lerp(x_fwd, x_home, t);
+      target.x = lerp(x_final, x_init, t);
+      target.y = lerp(y_final, y_init, t);
       target.z = z_floor;
     }
     // --- Swing Phase ----
     else {
       const double t = (limb_phase - duty_factor) / (1 - duty_factor);
-      target.x = bezier(x_home, x_home, x_fwd, x_fwd, t);
+      target.x = bezier(x_init, x_init, x_final, x_final, t);
+      target.y = bezier(y_init, y_init, y_final, y_final, t);
       target.z = bezier(z_floor, z_peak, z_peak, z_floor, t);
     }
-    target.y = config_.home_point.y;
     break;
   }
     // --- STAND MODE ---
